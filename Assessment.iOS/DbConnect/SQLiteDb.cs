ef637c59fed4f4bfc3cd29de1ab758a9e6acc366
Twinkle <124211977+TwinSrvs@users.noreply.github.com>
2023-02-03 08:12:08 +0530

    Add files via upload

---
+++ Assessment.iOS/DbConnect/SQLiteDb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Assessment.DbConnect;
+using Assessment.iOS.DbConnect;
+using SQLite;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+namespace Assessment.iOS.DbConnect
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        #region METHODS
+
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string path = Path.Combine(documentsPath, "MyEmployeeDb.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+
+        #endregion
+    }
+}
